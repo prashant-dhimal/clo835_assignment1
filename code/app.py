86cc7e8fc921d54e581c1edac721dc7577cac394
prashanttt <pacificdhimal@gmail.com>
2023-02-06 22:47:38 -0500

    Update app.py

--- code/app.py
+++ code/app.py
@@ -48,6 +48,10 @@ COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lim
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('addemp.html', color=color_codes[COLOR])
+@app.route("/pink", methods=['GET', 'POST'])
+def home():
+    return render_template('addemp.html', color=color_codes[pink])
+
 
 @app.route("/about", methods=['GET','POST'])
 def about():
